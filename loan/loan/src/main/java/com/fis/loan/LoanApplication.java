88beb57099c15e46a9a884d5ca58fdb0fe53d65c
Rishabh Kumar <work25860@gmail.com>
2021-08-16 15:42:55 +0530

    Adding update

--- loan/loan/src/main/java/com/fis/loan/LoanApplication.java
+++ loan/loan/src/main/java/com/fis/loan/LoanApplication.java
@@ -2,7 +2,9 @@ package com.fis.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient 
 @SpringBootApplication
 public class LoanApplication {
 
